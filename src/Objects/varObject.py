551a98f74aa4a8c6eb2331a8cfdd9adfab00ac5e
Hasnat Abdullah <abdullah.2010bd@gmail.com>
2019-04-09 02:24:39 +0600

    Delete varObject.py

--- src/Objects/varObject.py
+++
@@ -1,9 +0,0 @@
-
-
-class VariableObject(object):
-    def __init__(self):
-        self.exec_string = ""
-
-    def transpile(self, name, operator, value):
-        self.exec_string += name + " " + operator + " " + value
-        return self.exec_string
